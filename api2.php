0a97d6a3e107d21db335599b93bf1d0aeffd1147
Edistra <matthieu.desprez@gmail.com>
2017-03-23 22:57:34 +0100

    add plugin2.json

---
+++ api2.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Sample Gantter Extension
+ * Reads post variables and displays values
+ */
+//$projectXML = simplexml_load_string($_POST["projectXML"]);
+//
+//$sxe = new SimpleXMLElement($projectXML->asXML());
+//$sxe->registerXPathNamespace('n', 'http://schemas.microsoft.com/project');
+//
+//$result = $sxe->xpath('/n:Project/n:Tasks/n:Task[n:UID[text()="0"]]');
+//
+//$title = $result[0]->Name;
+
+header('Content-type: text/html');
+
+?>
+<html>
+<head>
+    <title>Gantter UI Extension Example</title>
+</head>
+
+<body>
+<h1>Gantter UI Extension Example</h1>
+
+
+</body>
+</html>
